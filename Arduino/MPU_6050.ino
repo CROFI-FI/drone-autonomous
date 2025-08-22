201778e0b31703e45f82c5b89caf488def0418db
Quique1409 <enriquemedranorabak@hotmail.com>
2025-08-22 15:26:30 -0600

    Se agrega carpeta arduino con los scripts para los MPU

---
+++ Arduino/MPU_6050.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.initialize();
+
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 no conectado correctamente");
+    while (1);
+  }
+
+  Serial.println("MPU6050 listo");
+}
+
+void loop() {
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  // Imprime en formato: ax,ay,az,gx,gy,gz
+  Serial.print(ax); Serial.print(",");
+  Serial.print(ay); Serial.print(",");
+  Serial.print(az); Serial.print(",");
+  Serial.print(gx); Serial.print(",");
+  Serial.print(gy); Serial.print(",");
+  Serial.println(gz);
+
+  delay(50); // 20 Hz
+}
